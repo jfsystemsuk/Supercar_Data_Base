18c67b497025906e3a113d8ed37ef8e462286ac3
jfsystemsuk <jfsystemsuk@yahoo.co.uk>
2020-04-29 13:37:39 +0000

    Added fact.html

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'Supercar_Data_Base'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-y0jyu.mongodb.net/Supercar_Data_Base?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_facts')
+def get_tasks():
+    return render_template("facts.html", facts=mongo.db.facts.find())
 
 
 if __name__ == '__main__':
