f4f7474d7260cbc8377f6c17b77772b58f0c5fa6
jfsystemsuk <jfsystemsuk@yahoo.co.uk>
2020-04-30 12:37:45 +0000

    Added empty Templates ready for code

--- app.py
+++ app.py
@@ -5,16 +5,21 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'Supercar_Data_Base'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-y0jyu.mongodb.net/Supercar_Data_Base?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-y0jyu.mongodb.net/Supercar_Data_Base?retryWrites=true&w=majority'  # noqa
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
 @app.route('/get_facts')
-def get_tasks():
+def get_facts():
     return render_template("facts.html", facts=mongo.db.facts.find())
 
+
+@app.route('/add_fact')
+def add_fact():
+    return render_template('addfact.html', supercars=mongo.db.supercars.find())   # noqa
+        
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
